dd377739a92e61d8d3515b9cc068ee0dcf97558b
AtanasovCode <a_ljupce@yahoo.com>
2025-07-01 17:33:52 +0200

    create theme & add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,15 @@ const App = () => {
   const { bears } = useDictionaryStore();
 
   return (
-    <div className="w-full min-h-dvh flex items-center justify-center bg-slate-900 text-white">
-      {bears}
+    <div className="w-full min-h-dvh flex items-center justify-center bg-background text-text">
+      <div className="flex flex-col items-center justify-center gap-3">
+        <div className="font-sans">
+          Sans Serif Text
+        </div>
+        <div className="font-serif text-5xl">
+          Serif Text
+        </div>
+      </div>
     </div>
   );
 }
