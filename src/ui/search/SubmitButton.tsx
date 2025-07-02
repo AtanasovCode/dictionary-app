e3c392c1c586b803569731c7c27eaf5a7e6b0c3e
AtanasovCode <a_ljupce@yahoo.com>
2025-07-02 18:10:18 +0200

    add API call & retrieve word data

--- src/ui/search/SubmitButton.tsx
+++ src/ui/search/SubmitButton.tsx
@@ -1,11 +1,42 @@
-
+import { useDictionaryStore } from "../../../useDictionaryStore";
 
 const SubmitButton = () => {
+
+    const {
+        word,
+        setDictionary,
+        setLoading,
+    } = useDictionaryStore();
+
+    const handleGetDictionary = async () => {
+        try {
+            setLoading(true);
+
+            const data = await fetch(` https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+
+            if (!data) {
+                console.error("Something went wrong");
+                return;
+            }
+
+            const dictionaryResponse = await data.json();
+            setDictionary(dictionaryResponse);
+
+
+            console.log(dictionaryResponse[0]);
+        } catch (error: any) {
+            console.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
     return (
-        <input 
+        <input
             type="button"
             value="Submit"
-            className="bg-accent rounded-md text-center text-text py-3 px-10 hover:cursor-pointer hover:bg-blue-500                       "
+            className="bg-accent rounded-md text-center text-text py-3 px-10 hover:cursor-pointer hover:bg-blue-500"
+            onClick={() => handleGetDictionary()}
         />
     );
 }
--- useDictionaryStore.tsx
+++ useDictionaryStore.tsx
@@ -3,9 +3,21 @@ import { create } from 'zustand'
 interface DictionaryState {
   word: string | null;
   setWord: (word: string) => void;
+
+  loading: boolean;
+  setLoading: (value: boolean) => void;
+
+  dictionary: string[];
+  setDictionary: (value: string[]) => void;
 }
 
 export const useDictionaryStore = create<DictionaryState>()((set) => ({
   word: null,
   setWord: (word: string) => set({ word }),
+
+  loading: false,
+  setLoading: (loading: boolean) => set({ loading }),
+
+  dictionary: [],
+  setDictionary: (value: string[]) => set({ dictionary: value }),
 }))
