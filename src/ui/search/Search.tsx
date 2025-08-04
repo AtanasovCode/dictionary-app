6031807674868ce6127b0090a60ccb2cb2aa188f
AtanasovCode <a_ljupce@yahoo.com>
2025-08-04 18:02:00 +0200

    transform word search to form with submit

--- src/ui/search/Search.tsx
+++ src/ui/search/Search.tsx
@@ -1,12 +1,50 @@
 import SearchBar from "./SearchBar";
 import SubmitButton from "./SubmitButton";
+import { useDictionaryStore } from "../../../useDictionaryStore";
 
 const Search = () => {
+
+    const {
+        word,
+        setDictionary,
+        setLoading,
+    } = useDictionaryStore();
+
+    const handleGetDictionary = async () => {
+        try {
+            setLoading(true);
+
+            const trimmedWord = word?.trim(); // trim word to avoid errors with empty spaces
+            const data = await fetch(` https://api.dictionaryapi.dev/api/v2/entries/en/${trimmedWord}`);
+
+            if (!data) {
+                console.error("Something went wrong");
+                return;
+            }
+
+            const dictionaryResponse = await data.json();
+            setDictionary(dictionaryResponse);
+
+
+            console.log(dictionaryResponse[0]);
+        } catch (error: any) {
+            console.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
     return (
-        <div className="flex items-stretch justify-between w-full p-1 rounded-md bg-secondary">
+        <form
+            onSubmit={(e) => {
+                e.preventDefault()
+                handleGetDictionary();
+            }}
+            className="flex items-stretch justify-between w-full p-1 rounded-md bg-secondary"
+        >
             <SearchBar />
             <SubmitButton />
-        </div>
+        </form>
     );
 }
 
--- src/ui/search/SubmitButton.tsx
+++ src/ui/search/SubmitButton.tsx
@@ -3,53 +3,13 @@ import Loading from "./Loading";
 import SearchIcon from "../../assets/search-icon.svg";
 
 const SubmitButton = () => {
-
-    const {
-        word,
-        setDictionary,
-        loading,
-        setLoading,
-    } = useDictionaryStore();
-
-    const handleGetDictionary = async () => {
-        try {
-            setLoading(true);
-
-            const data = await fetch(` https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
-
-            if (!data) {
-                console.error("Something went wrong");
-                return;
-            }
-
-            const dictionaryResponse = await data.json();
-            setDictionary(dictionaryResponse);
-
-
-            console.log(dictionaryResponse[0]);
-        } catch (error: any) {
-            console.error(error.message);
-        } finally {
-            setLoading(false);
-        }
-    }
-
     return (
-        <button
-            className="px-6 rounded-md bg-accent hover:bg-blue-500 flex items-center justify-center cursor-pointer"
-            onClick={() => handleGetDictionary()}
-        >
-            {
-                loading ? (
-                    <Loading />
-                ) : (
-                    <img
-                        src={SearchIcon}
-                        alt="search icon"
-                        className="h-8"
-                    />
-                )
-            }
+        <button type="submit" className="px-6 rounded-md bg-accent hover:bg-blue-500 flex items-center justify-center cursor-pointer">
+            <img
+                src={SearchIcon}
+                alt="search icon"
+                className="h-8"
+            />
         </button>
     );
 }
